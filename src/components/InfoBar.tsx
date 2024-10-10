9005c55e43233d213c0b8f2c79ef4971292ba319
simrandutt <85801110+simrandutt@users.noreply.github.com>
2024-10-10 10:58:23 -0400

    infobar animation

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/info.scss';
 
 const InfoBar: React.FC = () => {
+  const [isPaused, setIsPaused] = useState(false);
+
   return (
-    <div className="info-bar">
-      <div className="scrolling-text">
-        Next maintenance is planned between 02/11/2024 Saturday 10:00 AM ET to 03/11/2024 Sunday 10:00 PM ET . Please ensure that all systems are prepared for downtime.
+    <div 
+      className="info-bar" 
+      onMouseEnter={() => setIsPaused(true)}  
+      onMouseLeave={() => setIsPaused(false)}
+    >
+      <div className={`scrolling-text ${isPaused ? 'paused' : ''}`}>
+        Next maintenance is planned between 02/11/2024 Saturday 10:00 AM ET to 03/11/2024 Sunday 10:00 PM ET. Please ensure that all systems are prepared for downtime.
       </div>
     </div>
   );
